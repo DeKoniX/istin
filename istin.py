18c4677c37a380ac46a2eb75e26fbf17a71a7d82
DeKoniX <dekonix@gmail.com>
2013-09-11 23:43:14 +0700

    Initial commit

---
+++ istin.py
@@ -0,0 +1,34 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+#str = "(1->2)&(3<->1)"
+#stro = "1&2~3||-4"
+#stro = "1&2~3"
+#stro = "(A|C&-B)&(A|C)"
+#stro = "A|C&-B"
+stro = "A&B"
+znak = ('&', '|')
+m = []
+
+class istin:
+    def __init__(self, stro):
+        k = 0
+        for i in range(0, len(stro)):
+            for j
+        # k - количество символов
+        #k = len(stro)
+        #for i in range(0, len(znak)-1):
+            #if stro.find(znak[i]) != -1:
+                #k-=1
+        #self.go(stro, k)
+
+    #def go(self, stro, k):
+        #print stro
+        #print k
+
+        #for i in range(0, len(stro)):
+            #m.append(stro[i])
+
+istin(stro)
+
+
