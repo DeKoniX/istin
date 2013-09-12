7fa77bbad1d725396af68a4d640433109070e257
DeKoniX <dekonix@gmail.com>
2013-09-12 11:51:53 +0700

    Массив строк

--- istin.py
+++ istin.py
@@ -6,15 +6,64 @@
 #stro = "1&2~3"
 #stro = "(A|C&-B)&(A|C)"
 #stro = "A|C&-B"
-stro = "A&B"
-znak = ('&', '|')
 m = []
+znak = ('&', '|')
+stro = "A&B|C"
 
 class istin:
     def __init__(self, stro):
-        k = 0
-        for i in range(0, len(stro)):
-            for j
+        #stroki = pow(2, len(self.simbol(stro)))
+        m = self.massstrok(stro)
+        for s in stro:
+            if s == "-":
+                stro,  = otric()
+
+
+    def simbol(self, stro):
+        for i in stro:
+            est = False
+            for z in znak:
+                if i == z:
+                    est = True
+            for j in m:
+                if j == i:
+                    est = True
+            if est == False:
+                m.append(i)
+        return m
+
+    def massstrok(self, stro):
+        mass = []
+        j = 0
+        m = self.simbol(stro)
+        d = pow(2, len(m))
+        t = d
+        for z in m:
+            mass.append([z])
+            t = t/2
+            tt = 0
+            for i in range(1, d+1):
+                tt += 1
+                if tt<=t:
+                    mass[j].append(0)
+                elif tt>t:
+                    mass[j].append(1)
+                    if tt == t*2:
+                        tt=0
+            #for i in range(0, d):
+                #print "><"+str(i)
+                #for nu in range(0, d):
+                    #mass[j].append(0)
+                #for nu in range(0, d):
+                    #mass[j].append(1)
+                #if i <= d:
+                    #mass[j].append(0)
+                #else:
+                    #mass[j].append(1)
+            j+=1
+        return mass
+
+
         # k - количество символов
         #k = len(stro)
         #for i in range(0, len(znak)-1):
@@ -30,5 +79,3 @@ class istin:
             #m.append(stro[i])
 
 istin(stro)
-
-
