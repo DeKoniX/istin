80134bd95d7b948102cd039eb29973f9a740148e
DeKoniX <dekonix@gmail.com>
2013-09-15 20:34:39 +0700

    Сделал, вроде все, и вроде даже работает *_*

--- istin.py
+++ istin.py
@@ -2,49 +2,136 @@
 # -*- coding: utf-8 -*-
 # DeKoniX =^_^= 2013
 
-#str = "(1->2)&(3<->1)"
-#stro = "1&2~3||-4"
-#stro = "1&2~3"
-#stro = "(A|C&-B)&(A|C)"
-#stro = "A|C&-B"
 m = []
-znak = ('&', '|', '-')
-stro = "A&B|-C"
-#stro = "P&Q|R&P"
+znak = ('&', '|', '-', '>', '~', '(', ')')
+stro = "(-(A&B)|(C&B&A>-(A&-C~B&C)))"
 
 class istin:
     def __init__(self, stro):
-        #stroki = pow(2, len(self.simbol(stro)))
-        m = self.massstrok(stro)
-        otv = []
-        i_mass = 0
         print stro
+        i_mass = 0
+        otv = []
+        m = self.massstrok(stro)
+        self.go(stro, i_mass, otv, m)
+
+    def go(self, stro, i_mass, otv, m):
+        while stro.find('(') != -1:
+            k = 0
+            for i in range(0, len(stro)):
+                if stro[i] == '(':
+                    k += 1
+                    if k == 1:
+                        str_begin = i
+                if stro[i] == ')':
+                    k -= 1
+                    if k == 0:
+                        str_end = i
+            otv, i_mass = self.go(stro[str_begin+1:str_end], i_mass, otv, m)
+            stro = stro.replace(stro[str_begin:str_end+1], str(i_mass-1))
+
         # Отрицание
-        for i in range(0, len(stro)):
+        i = 0
+        while i<len(stro):
             if stro[i] == "-":
                 # ToDo: передовать не следующий символ, а смотреть сколько символов, для чисел больше 9
                 otv = self.otric(stro[i+1], m, otv, i_mass)
-                stro = stro.replace(stro[i+1], str(i_mass))
-                stro = stro.replace(stro[i], '')
+                stro = stro.replace(stro[i+1], str(i_mass), 1)
+                stro = stro.replace(stro[i], '', 1)
                 i_mass += 1
-                break
+                i -= 1
+            i += 1
+        print otv
         # Конъюнкция
-        for i in range(0, len(stro)):
+        i = 0
+        while i<len(stro):
             if stro[i] == "&":
                 # ToDo: передовать не следующий символ, а смотреть сколько символов, для чисел больше 9
                 otv = self.conjunction(stro[i-1], stro[i+1], m, otv, i_mass)
-                stro = stro.replace(stro[i-1:i+2], str(i_mass))
+                stro = stro.replace(stro[i-1:i+2], str(i_mass), 1)
                 i_mass += 1
-                break
+                i -= 2
+            i += 1
+        print otv
         # Дизъюнкция
-        for i in range(0, len(stro)):
+        i = 0
+        while i<len(stro):
             if stro[i] == "|":
                 # ToDo: передовать не следующий символ, а смотреть сколько символов, для чисел больше 9
                 otv = self.disjunction(stro[i-1], stro[i+1], m, otv, i_mass)
                 stro = stro.replace(stro[i-1:i+2], str(i_mass))
                 i_mass += 1
-                break
+                i -= 2
+            i += 1
         print otv
+        # Импликация
+        i = 0
+        while i<len(stro):
+            if stro[i] == ">":
+                # ToDo: передовать не следующий символ, а смотреть сколько символов, для чисел больше 9
+                otv = self.implication(stro[i-1], stro[i+1], m, otv, i_mass)
+                stro = stro.replace(stro[i-1:i+2], str(i_mass))
+                i_mass += 1
+                i -= 2
+            i += 1
+        print otv
+        # Эквиваленция
+        i = 0
+        while i<len(stro):
+            if stro[i] == "~":
+                # ToDo: передовать не следующий символ, а смотреть сколько символов, для чисел больше 9
+                otv = self.equality(stro[i-1], stro[i+1], m, otv, i_mass)
+                stro = stro.replace(stro[i-1:i+2], str(i_mass))
+                i_mass += 1
+                i -= 2
+            i += 1
+        return otv, i_mass
+
+    def equality(self, a, b, m, otv, i_mass):
+        print a+"~"+b
+        try:
+            mass1 = otv[int(a)]
+        except ValueError:
+            for s in m:
+                if s[0] == a:
+                    mass1 = s
+            mass1 = mass1[1:]
+
+        try:
+            mass2 = otv[int(b)]
+        except ValueError:
+            for s in m:
+                if s[0] == b:
+                    mass2 = s
+            mass2 = mass2[1:]
+        otv.append([])
+        for i in range(0, len(mass1)):
+            otv[i_mass].append(int(int(mass1[i]) == int(mass2[i])))
+            print str(mass1[i])+"~"+str(mass2[i])+" = "+str(int(int(mass1[i]) == int(mass2[i])))
+        return otv
+
+
+    def implication(self, a, b, m, otv, i_mass):
+        print a+">"+b
+        try:
+            mass1 = otv[int(a)]
+        except ValueError:
+            for s in m:
+                if s[0] == a:
+                    mass1 = s
+            mass1 = mass1[1:]
+
+        try:
+            mass2 = otv[int(b)]
+        except ValueError:
+            for s in m:
+                if s[0] == b:
+                    mass2 = s
+            mass2 = mass2[1:]
+        otv.append([])
+        for i in range(0, len(mass1)):
+            otv[i_mass].append(int(int(mass1[i]) > int(mass2[i])))
+            print str(mass1[i])+">"+str(mass2[i])+" = "+str(int(int(mass1[i]) > int(mass2[i])))
+        return otv
 
 
     def disjunction(self, a, b, m, otv, i_mass):
@@ -55,20 +142,19 @@ class istin:
             for s in m:
                 if s[0] == a:
                     mass1 = s
+            mass1 = mass1[1:]
+
         try:
             mass2 = otv[int(b)]
         except ValueError:
             for s in m:
                 if s[0] == b:
                     mass2 = s
+            mass2 = mass2[1:]
         otv.append([])
-        print range(1, len(mass1))
         for i in range(0, len(mass1)):
-            try:
-                otv[i_mass].append(int(int(mass1[i]) | int(mass2[i])))
-                print str(mass1[i])+"|"+str(mass2[i])+" = "+str(int(int(mass1[i]) | int(mass2[i])))
-            except:
-                pass
+            otv[i_mass].append(int(int(mass1[i]) | int(mass2[i])))
+            print str(mass1[i])+"|"+str(mass2[i])+" = "+str(int(int(mass1[i]) | int(mass2[i])))
         return otv
 
     def conjunction(self, a, b, m, otv, i_mass):
@@ -79,20 +165,21 @@ class istin:
             for s in m:
                 if s[0] == a:
                     mass1 = s
+            mass1 = mass1[1:]
+
         try:
-            mass2 = otv[int(a)]
+            mass2 = otv[int(b)]
         except ValueError:
             for s in m:
                 if s[0] == b:
                     mass2 = s
+            mass2 = mass2[1:]
+        print mass1
+        print mass2
         otv.append([])
-        print range(1, len(mass1))
         for i in range(0, len(mass1)):
-            try:
-                otv[i_mass].append(int(int(mass1[i]) & int(mass2[i])))
-                print str(mass1[i])+"&"+str(mass2[i])+" = "+str(int(int(mass1[i]) & int(mass2[i])))
-            except:
-                pass
+            otv[i_mass].append(int(int(mass1[i]) & int(mass2[i])))
+            print str(mass1[i])+"&"+str(mass2[i])+" = "+str(int(int(mass1[i]) & int(mass2[i])))
         return otv
 
     def otric(self, a, m, otv, i_mass):
@@ -103,13 +190,11 @@ class istin:
             for s in m:
                 if s[0] == a:
                     mass1 = s
+            mass1 = mass1[1:]
         otv.append([])
-        for i in range(1, len(mass1)):
-            try:
-                otv[i_mass].append(int(not int(mass1[i])))
-                print "-"+str(mass1[i])+" = "+str(int(not int(mass1[i])))
-            except:
-                pass
+        for i in range(0, len(mass1)):
+            otv[i_mass].append(int(not int(mass1[i])))
+            print "-"+str(mass1[i])+" = "+str(int(not int(mass1[i])))
         return otv
 
     def simbol(self, stro):
@@ -143,32 +228,6 @@ class istin:
                     mass[j].append(1)
                     if tt == t*2:
                         tt=0
-            #for i in range(0, d):
-                #print "><"+str(i)
-                #for nu in range(0, d):
-                    #mass[j].append(0)
-                #for nu in range(0, d):
-                    #mass[j].append(1)
-                #if i <= d:
-                    #mass[j].append(0)
-                #else:
-                    #mass[j].append(1)
             j+=1
         return mass
-
-
-        # k - количество символов
-        #k = len(stro)
-        #for i in range(0, len(znak)-1):
-            #if stro.find(znak[i]) != -1:
-                #k-=1
-        #self.go(stro, k)
-
-    #def go(self, stro, k):
-        #print stro
-        #print k
-
-        #for i in range(0, len(stro)):
-            #m.append(stro[i])
-
-istin(stro)
+#istin(stro)
